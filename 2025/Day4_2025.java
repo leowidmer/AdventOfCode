3079e741c88aedc4fc37a538466409406602d17f
leowidmer <leodylan02@gmail.com>
2025-12-08 16:25:42 +0100

    Complete Day 4

---
+++ 2025/Day4_2025.java
@@ -0,0 +1,58 @@
+public class Day4_2025 {
+
+    private static char[][] input;
+
+    public static void main(String[] args) {
+        input = InputParser.getMatrix();
+        System.out.println("Task 1 Result: " + task1(input));
+        System.out.println("Task 2 Result: " + task2(input));
+
+    }
+
+
+    private static long task1(char[][] input) {
+        long total = 0;
+        for (int i = 1; i < input.length; i++) {
+            for (int j = 1; j < input[i].length; j++) {
+                if (input[i][j] == '@' && isAccessible(i,j)) {
+                    total++;
+                }
+            }
+        }
+        return total;
+    }
+
+    private static long task2(char[][] input) {
+        long total = 0;
+        boolean matrixChanged = true;
+        while (matrixChanged) {
+            matrixChanged = false;
+            for (int i = 1; i < input.length; i++) {
+                for (int j = 1; j < input[i].length; j++) {
+                    if (input[i][j] == '@' && isAccessible(i,j)) {
+                        input[i][j] = '.';
+                        total++;
+                        matrixChanged = true;
+                    }
+                }
+            }
+        }
+
+        return total;
+    }
+
+    private static boolean isAccessible(int i, int j) {
+        int count = 0;
+        // you may not like it but this is what peak programming looks like
+        if (input[i - 1][j] == '@') count++;
+        if (input[i + 1][j] == '@') count++;
+        if (input[i][j - 1] == '@') count++;
+        if (input[i][j + 1] == '@') count++;
+        if (input[i - 1][j - 1] == '@') count++;
+        if (input[i + 1][j + 1] == '@') count++;
+        if (input[i - 1][j + 1] == '@') count++;
+        if (input[i + 1][j - 1] == '@') count++;
+
+        return count < 4;
+    }
+}
--- 2025/InputParser.java
+++ 2025/InputParser.java
@@ -3,6 +3,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class InputParser {
@@ -22,6 +23,16 @@ public class InputParser {
         List<String> lines = readLines();
         String[] splits = lines.getFirst().split(splitPattern);
         return Arrays.asList(splits);
+    }
+
+    public static char[][] getMatrix() {
+        List<String> lines = readLines();
+        char[][] matrix = new char[lines.size()+2][lines.getFirst().length()+2];
+        for(int i = 0; i < lines.size(); i++) {
+            lines.set(i,'\0' + lines.get(i) + '\0');
+            matrix[i+1] = lines.get(i).toCharArray();
+        }
+        return matrix;
     }
 
 }
